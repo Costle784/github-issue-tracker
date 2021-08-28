a696559bf6095c9c9cc5e6d54b06c0f526aa43db
Curtis Ostle <curtisostle@gmail.com>
2021-08-28 11:24:47 -0400

    Create react app; cleanup files

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import "./app.css";
+
+function App() {
+    return (
+        <div className="app">
+            <header className="app-header">Github Issue Tracker</header>
+        </div>
+    );
+}
+
+export default App;
